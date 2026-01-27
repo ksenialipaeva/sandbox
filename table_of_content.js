151e13100f5c1168e3abda3d32b8dc0ce6bdcbf8
AlenaAW <alena.y.sidorenko@gmail.com>
2026-01-27 22:30:17 +0300

    Содержание начало

--- table_of_content.js
+++ table_of_content.js
@@ -1,6 +1,8 @@
 //Собираем все заголовки h2 в список
 let h2_content = document.getElementsByTagName('h2');
+
 let productList = [];
+let h1 = document.getElementsByTagName('h1')[0];
 
 for (let j=0;j<h2_content.length;j++){
     h2_content[j].id = j;
@@ -8,10 +10,8 @@ for (let j=0;j<h2_content.length;j++){
 }
  
 let ul = document.createElement('ul');
-let content_table=document.createElement('div');
-content_table.classList.add('content_table');
-body.prepend(content_table);
-content_table.appendChild(ul);
+ul.classList.add('content_table');
+h1.after(ul);
 productList.forEach(renderProductList);
        
 //добавляем заголовки h2 в список
@@ -23,3 +23,21 @@ function renderProductList(element, index) {
     a.innerHTML=a.innerHTML + element;
     a.href = "#"+index;
 }
+//подсвечиваем главы по мере прокрутки
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const elHeaders = document.querySelectorAll('h2');
+    let headerId = '';
+    for (let i = elHeaders.length - 1; i >= 0; i--) {
+      if (elHeaders[i].getBoundingClientRect().top + window.scrollY - 200 < scrollTop) {
+        headerId = elHeaders[i].id;
+        break;
+      }
+    }
+    document.querySelectorAll('.content_table a.active').forEach(el => {
+      el.classList.remove('active');
+    });
+    if (headerId) {
+      document.querySelector(`a[href$="#${headerId}"]`).classList.add('active');
+    }
+  });
